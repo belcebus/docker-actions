9e9befb9f49c76d3fe354e1e61af9ce83810e9b8
David Hernández Martín <belcebus@gmail.com>
2021-08-28 16:37:51 +0200

    testing

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:alpine3.14
-
-WORKDIR /scripts
-COPY scripts/ .
-
-ENTRYPOINT [ "python", "/scripts/hello-world.py"]
--- scripts/hello-world.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument("--nombre",help="Nombre a saludar",type=str,default="tu",required=False,)
-
-args = parser.parse_args()
-
-print(f"Hola {args.nombre}")
